fa98ba107bd620a714ad229cea5bd652d719f539
Ulrik Røsby <ulrik@rosby.no>
2021-06-27 23:37:53 +0200

    Make barebones successes page

---
+++ components/SuccessesForm/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const SuccessesForm: FC = (props) => {
+  return <div>This is a form</div>;
+};
+
+export default SuccessesForm;
--- pages/successes.tsx
+++ pages/successes.tsx
@@ -1,7 +1,22 @@
-import { FC } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { FC, useContext } from 'react';
+import ListPage from '../components/ListPage';
+import SuccessesForm from '../components/SuccessesForm/';
+import { SuccessGroupsContext } from '../lib/contexts';
+
 
 const Successes: FC = () => {
-  return <div>Successes</div>;
+  const { successGroups } = useContext(SuccessGroupsContext);
+
+  return (
+    <ListPage
+      title="Combos"
+      description="Register combo when either of these scenarios occur."
+      popupTitle="Add combo"
+      infoList={successGroups.main}
+      addForm={<SuccessesForm />}
+    />
+  );
 };
 
 export default Successes;
