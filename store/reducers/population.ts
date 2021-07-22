62b29109d32f6477e09dde6a4a399b55cc8ac190
Ulrik Røsby <ulrik@rosby.no>
2021-07-23 00:48:10 +0200

    Update redux setup minimally

--- store/reducers/population.ts
+++ store/reducers/population.ts
@@ -1,6 +1,9 @@
-import { Element, Population } from "@prisma/client";
-import { AnyAction, Reducer } from "redux";
+import { Element } from '@prisma/client';
+import { AnyAction, Reducer } from 'redux';
 
 export const populationReducer: Reducer = (state: Element[] = [], action: AnyAction) => {
-  
-}
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
--- store/reducers/scenario-group.ts
+++ store/reducers/scenario-group.ts
@@ -1,12 +1,17 @@
 import { Element } from '.prisma/client';
 import { AnyAction, Reducer } from 'redux';
 
-const scenarioGroupReducer: Reducer = (state: Element[][], action: AnyAction) => {};
-
-export const successesReducer: Reducer = (state: Element[][], action: AnyAction) => {
-  scenarioGroupReducer(state, action);
+const scenarioGroupReducer: Reducer = (state: Element[][], action: AnyAction) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
-export const failuresReducer: Reducer = (state: Element[][], action: AnyAction) => {
-  scenarioGroupReducer(state, action);
+export const successesReducer: Reducer = (state: Element[][] = [], action: AnyAction) => {
+  return scenarioGroupReducer(state, action);
+};
+
+export const failuresReducer: Reducer = (state: Element[][] = [], action: AnyAction) => {
+  return scenarioGroupReducer(state, action);
 };
